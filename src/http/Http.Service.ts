51f8c9d03f05390fdcb34082443c9e5ef849f33e
BenSegniBBC <ben.segni@bbc.co.uk>
2024-06-27 15:29:53 +0100

    converted class to object for http

--- src/http/Http.Service.ts
+++ src/http/Http.Service.ts
@@ -2,20 +2,22 @@ import axios, { AxiosResponse } from "axios";
 import { config } from "./config/config";
 import { TeamMessageEnum } from "../components/teams/enum/team.message.enum";
 
-export class HttpService {
-    private static url = `${config.url}${config.port}`;
+export const HttpService = {
+    url: `${config.url}${config.port}`,
 
-    public static get(address: string): Promise<AxiosResponse> {
+    get(address: string): Promise<AxiosResponse> {
         return axios.get(`${HttpService.url}/${address}`);
-    }
+    },
 
-    public static getById(address: string, id: number): Promise<AxiosResponse> {
+    getById(address: string, id: number): Promise<AxiosResponse> {
         return axios.get(`${HttpService.url}/${address}/${id}`);
-    }
+    },
 
-    public static isError(error: unknown): boolean {
+    isError(error: unknown): boolean {
         return axios.isAxiosError(error)
-    }
+    },
 
-    public static address = TeamMessageEnum.address;
+    address: TeamMessageEnum.address
 }
+
+export default HttpService;
